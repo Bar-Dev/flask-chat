6384114200daa1e8c614567204e0d062b84bffb7
Bar-Dev <bdprojectservices@gmail.com>
2021-01-12 16:13:10 +0000

    Added chat textbox function

--- run.py
+++ run.py
@@ -1,21 +1,47 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
 
-@app.route('/')
+
+def add_message(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
+
+
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with Instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
 
 
-@app.route('/<username>')
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("index.html")
+
+
+@app.route('/chat/<username>', methods = ["GET", "POST"])
 def user(username):
-    return "Hi " + username
+    """Add and display chat messages"""
 
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+
+
+    return render_template(
+        "chat.html", username = username, chat_messages = messages)
+
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
 
